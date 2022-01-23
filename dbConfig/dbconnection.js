4b162c4f0024a35f4a0eb897e74ab1b12fb6c135
Kapil Tailor <kapil.tailor@connectlab.ai>
2022-01-23 17:54:04 +0530

    dbhelper

--- dbConfig/dbconnection.js
+++ dbConfig/dbconnection.js
@@ -5,8 +5,7 @@ module.exports.getConnectionObject = () => {
     host: process.env.DATABASE_HOST || '127.0.0.1',
     user: process.env.DATABASE_USER || 'root',
     password: process.env.DATABASE_PASSWORD || '',
-    database: process.env.DATABASE_NAME || 'the-virtual-classroom',
-    port: Number(process.env.PORT) || 3306
+    database: process.env.DATABASE_NAME || 'the-virtual-classroom'
   });
   connection.connect(function (err) {
     if(err){
