891159af9d021f288572c4bbc7b3be9cddefc7a1
Kapil Tailor <kapil.tailor@connectlab.ai>
2022-01-23 16:57:00 +0530

    port

--- dbConfig/dbconnection.js
+++ dbConfig/dbconnection.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql')
 
 module.exports.getConnectionObject = () => {
-  var connection = mysql.createConnection({
+  var connection = mysql.createPool({
     host: process.env.DATABASE_HOST || '127.0.0.1',
     user: process.env.DATABASE_USER || 'root',
     password: process.env.DATABASE_PASSWORD || '',
     database: process.env.DATABASE_NAME || 'the-virtual-classroom',
     port: Number(process.env.PORT) || 3306
-  });
-  connection.connect()
+  })
   return connection
 }
