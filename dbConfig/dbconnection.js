3fedf522c4e1a0ad3c1e57113e7f942b7a95e348
Kapil Tailor <kapil.tailor@connectlab.ai>
2022-01-30 19:05:05 +0530

    fix

--- dbConfig/dbconnection.js
+++ dbConfig/dbconnection.js
@@ -3,12 +3,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports =  new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+  dialect: 'postgres'
 })
